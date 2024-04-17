ed9d602947f1bebfc93d8a6e5fb26d2a54b5f9d7
Kartik Kaushik <kartik14101997@gamil.com>
2024-04-17 10:22:04 +0530

    Adding products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:HomeComponent
+    },
+    {
+        path:"about-us",
+        component:AboutUsComponent
+    }
+];
---
+++ src/app/pages/about-us/about-us.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../common/header/header.component';
+
+
+@Component({
+  selector: 'app-about-us',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.css'
+})
+export class AboutUsComponent {
+
+}
